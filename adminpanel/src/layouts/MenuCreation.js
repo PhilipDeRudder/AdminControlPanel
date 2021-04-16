dfcff7e19084875613d4f371bf7a2c10db71b511
n9haan00 <n9haan00@students.oamk.fi>
2021-04-16 13:53:33 +0300

    menucreation

---
+++ adminpanel/src/layouts/MenuCreation.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {useState} from "react";
+
+
+
+ const MenuCreation = (props) => {
+
+const [inputField, setInputfield] = useState([
+  {lunchName: '', askKitchen: '', allergens:'', availability: '', price:''},
+]);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("InputFields", inputField);
+}
+
+const handleAddfields = () => {
+  setInputfield([...inputField, {lunchName: '', askKitchen: '', allergens:'', availability:'', price:''}])
+}
+
+const handleRemoveFields = (index) => {
+  const values = [...inputField];
+  values.splice(index, 1);
+  setInputfield(values);
+}
+
+const handleChangeInput = (index, event) => {
+  const values = [...inputField];
+  values[index][event.target.name] = event.target.value;
+  setInputfield(values);
+  console.log(index, event.target.name)
+}
+
+  return (
+    <form style={{margin:'50px'}} onSubmit={handleSubmit}>
+    <h1>Days menu</h1>
+    { inputField.map((inputField, index)=>(
+      <div key={index}>
+        <input type="text"
+        name="lunchName"
+        placeholder="Lunch name"
+        value={inputField.lunchName}
+        onChange={event => handleChangeInput(index, event)}/>
+        
+        <select name="allergens"
+         value={inputField.allergens}
+         onChange={event => handleChangeInput(index, event)}>
+           <option>1</option>
+           <option>2</option>
+         </select>
+
+        <input type="text"
+        name="askKitchen"
+        placeholder="Ask from kitchen"
+        value={inputField.askKitchen}
+        onChange={event => handleChangeInput(index, event)} />
+        
+        <input type="number"
+        name="availability"
+        placeholder="availability"
+        value={inputField.availability}
+        onChange={event => handleChangeInput(index, event)} />
+        
+        <input type="number"
+        name="Price"
+        placeholder="Price"
+        value={inputField.price}
+        onChange={event => handleChangeInput(index, event)} />
+         
+
+        <button onClick={() => handleRemoveFields(index)}>remove</button>
+        <button onClick={() => handleAddfields()}>add</button>
+      </div>
+    ))}
+    <button onClick={handleSubmit}>save</button>
+    </form>
+  );
+}
+
+export default MenuCreation;
